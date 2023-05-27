9c7ffbb8445be7e2557d0b434fc32759906d96c6
ProjetmemoRPG <131049108+ProjetmemoRPG@users.noreply.github.com>
2023-05-27 22:40:55 +0200

    Update mapper.h

--- final-main1 (10)/mapper.h
+++ final-main1 (10)/mapper.h
@@ -1,14 +1,14 @@
 #include "base.h"
 
 
-void resetMap(carte** map1, carte** map2);
+void resetMap(carte** map1, carte** map2);//reset la carte originale
 
-void afichage(carte** world,player* plP);
+void afichage(carte** world,player* plP); //print la carte 
 
-carte** mapSpawning(int n);
+carte** mapSpawning(int n);//return un taleau de n par n
 
-void itemList_creator(item itemList[LARG*LARG]);
+void itemList_creator(item itemList[LARG*LARG]);//crée tout les élément a placer dans le tableau
 
-void mapFeeding(carte** map,int nMap,item* itemList,int nList);
+void mapFeeding(carte** map,int nMap,item* itemList,int nList);//remplie le tableau de magnère aléatoire
 
-void free_map(carte** map);
+void free_map(carte** map);//libère les alocation faite lors du mapSpawning
